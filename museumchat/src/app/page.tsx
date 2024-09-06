6059f66e4900b36c85375c6dcc6e853ccc350e66
deonmenezes <150112885+deonmenezes@users.noreply.github.com>
2024-09-06 09:45:02 +0530

    lets do it

--- museumchat/src/app/page.tsx
+++ museumchat/src/app/page.tsx
@@ -54,7 +54,7 @@ const Chatbot: React.FC = () => {
       chooseLanguage:
         "Please select your preferred language to start the chat:",
       buttonLabel: "Send",
-      languages: ["English", "हिन्दी", "मराठी"],
+      languages: ["English", "हिन्दी", "मराठी "],
     },
     hindi: {
       welcome:
